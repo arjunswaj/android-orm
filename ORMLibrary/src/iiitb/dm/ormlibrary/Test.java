722a2e4f84f88963ed66d470abcd876ce6326570
Abhijith Madhav <browseanddownload@gmail.com>
2014-02-16 12:25:35 +0530

    Initial commit

---
+++ ORMLibrary/src/iiitb/dm/ormlibrary/Test.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package iiitb.dm.ormlibrary;
+
+/**
+ * @author kempa
+ *
+ */
+public class Test
+{
+	public String print()
+	{
+		return "Hello. This is the library kicking in :)";
+	}
+
+}
