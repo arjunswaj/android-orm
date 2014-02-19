7e1985a2ba03996f55876b1953795b23e696ff11
kumudinikakwani <kumudinikakwani13@gmail.com>
2014-02-18 23:15:05 -0500

    Private members accessed by getter methods.

--- ORMLibrary/src/iiitb/dm/ormlibrary/ORMHelper.java
+++ ORMLibrary/src/iiitb/dm/ormlibrary/ORMHelper.java
@@ -9,8 +9,10 @@ import iiitb.dm.ormlibrary.scanner.ClassScanner;
 import iiitb.dm.ormlibrary.scanner.ScanResult;
 import iiitb.dm.ormlibrary.scanner.impl.AnnotationsScannerImpl;
 import iiitb.dm.ormlibrary.scanner.impl.ClassScannerImpl;
+import iiitb.dm.ormlibrary.utils.Utils;
 
 import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,6 +25,7 @@ import android.content.pm.PackageManager.NameNotFoundException;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
 
 /**
  * ORM Helper
@@ -32,78 +35,81 @@ import android.database.sqlite.SQLiteOpenHelper;
  */
 public class ORMHelper extends SQLiteOpenHelper {
 
-  Context context;
-  private DMLQueryBuilder dmlQueryBuilder;
-  Map<Class, List<FieldValue>> scannedClassesFieldsMap;
-  Map<Class, String> scannedClassesTableMap;
-  ClassScanner scanner;
+	Context context;
+	private DMLQueryBuilder dmlQueryBuilder;
+	Map<Class, List<FieldValue>> scannedClassesFieldsMap;
+	Map<Class, String> scannedClassesTableMap;
+	ClassScanner scanner;
 
-  List<ClassDetails> classDetails = null;
+	List<ClassDetails> classDetails = null;
 
-  AnnotationsScanner annotationsScanner = new AnnotationsScannerImpl();
+	AnnotationsScanner annotationsScanner = new AnnotationsScannerImpl();
 
-  public ORMHelper(Context context, String name, CursorFactory factory,
-      int version) {
-    super(context, name, factory, version);
-    this.context = context;
-//    classDetails = annotationsScanner.getEntityObjectDetails(this.context);
-    dmlQueryBuilder = new DMLQueryBuilderImpl();
-    scannedClassesFieldsMap = new HashMap<Class, List<FieldValue>>();
-    scannedClassesTableMap = new HashMap<Class, String>();
-    scanner = new ClassScannerImpl();
-  }
+	public ORMHelper(Context context, String name, CursorFactory factory,
+			int version) {
+		super(context, name, factory, version);
+		this.context = context;
+		//    classDetails = annotationsScanner.getEntityObjectDetails(this.context);
+		dmlQueryBuilder = new DMLQueryBuilderImpl();
+		scannedClassesFieldsMap = new HashMap<Class, List<FieldValue>>();
+		scannedClassesTableMap = new HashMap<Class, String>();
+		scanner = new ClassScannerImpl();
+	}
 
-  private String getEOPackage() throws NameNotFoundException {
-    ApplicationInfo ai = context.getPackageManager().getApplicationInfo(
-        context.getPackageName(), PackageManager.GET_META_DATA);
-    String ormPackage = (String) ai.metaData.get("ormPackage");
-    context.getResources();
-    return ormPackage;
-  }
-  
-  
-  public void persist(Object obj)
-  {
-	  
-	  Class objClass = obj.getClass();
-	  
-	  if(scannedClassesFieldsMap.containsKey(objClass) == false)
-	  {
-		  ScanResult result = scanner.scan(objClass);
-		  scannedClassesFieldsMap.put(objClass, result.getFieldValues());
-		  scannedClassesTableMap.put(objClass, result.getTableName());
-		  
-	  }
-	  List<FieldValue> fieldValues = new ArrayList<FieldValue>(scannedClassesFieldsMap.get(objClass));
-	  for(FieldValue fieldValue : fieldValues)
-	  {
-		  try {
-			fieldValue.getField().setAccessible(true);
-			fieldValue.setFieldValue(fieldValue.getField().get(obj).toString());
-			
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		}catch(Exception e){
-			e.printStackTrace();
+	private String getEOPackage() throws NameNotFoundException {
+		ApplicationInfo ai = context.getPackageManager().getApplicationInfo(
+				context.getPackageName(), PackageManager.GET_META_DATA);
+		String ormPackage = (String) ai.metaData.get("ormPackage");
+		context.getResources();
+		return ormPackage;
+	}
+
+
+	public void persist(Object obj)
+	{
+
+		Class objClass = obj.getClass();
+
+		if(scannedClassesFieldsMap.containsKey(objClass) == false)
+		{
+			ScanResult result = scanner.scan(objClass);
+			scannedClassesFieldsMap.put(objClass, result.getFieldValues());
+			scannedClassesTableMap.put(objClass, result.getTableName());
+
 		}
-	  }
-	  String insertQuery = dmlQueryBuilder.generateInsertQuery(scannedClassesTableMap.get(objClass), fieldValues);
-	  
-	  getWritableDatabase().execSQL(insertQuery);
-  }
+		List<FieldValue> fieldValues = new ArrayList<FieldValue>(scannedClassesFieldsMap.get(objClass));
+		for(FieldValue fieldValue : fieldValues)
+		{
+			try {
+				
+				String getterMethodName = Utils.getGetterMethodName(fieldValue.getField().getName());
+				Method getterMethod = objClass.getMethod(getterMethodName);
+				fieldValue.setFieldValue(getterMethod.invoke(obj).toString());
 
-  @Override
-  public void onCreate(SQLiteDatabase arg0) {
-    // TODO Auto-generated method stub
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			}catch(Exception e){
+				e.printStackTrace();
+			}
+		}
+		String insertQuery = dmlQueryBuilder.generateInsertQuery(scannedClassesTableMap.get(objClass), fieldValues);
 
-  }
+		getWritableDatabase().execSQL(insertQuery);
+	}
 
-  @Override
-  public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-    // TODO Auto-generated method stub
 
-  }
+	@Override
+	public void onCreate(SQLiteDatabase arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
---
+++ ORMLibrary/src/iiitb/dm/ormlibrary/utils/Utils.java
@@ -0,0 +1,20 @@
+package iiitb.dm.ormlibrary.utils;
+
+import android.util.Log;
+
+public class Utils {
+	
+	public static String getGetterMethodName(String field)
+	{
+		String methodName = "get" + field.substring(0,1).toUpperCase() + field.substring(1);
+		Log.d("ADebugTag", "Method name is:" + methodName);
+		return methodName;
+	}
+
+	public static String getBooleanGetterMethodName(String field)
+	{
+		String methodName = "is" + field.substring(0,0).toUpperCase() + field.substring(1);
+		return methodName;
+	}
+
+}
