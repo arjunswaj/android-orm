334c51f013d3b02f7cbe102e64bcafb9be721058
Arjun S Bharadwaj <arjun.s.waj@gmail.com>
2014-03-12 19:45:07 +0530

    Support for OneToOne and OneToMany Unidirectional Mapping.

---
+++ ORMLibraryTest/src/edu/iiitb/ormtestapp/composition/eo/Country.java
@@ -0,0 +1,58 @@
+package edu.iiitb.ormtestapp.composition.eo;
+
+import java.util.Collection;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+
+@Entity(name = "COUNTRY")
+public class Country {
+  @Id
+  @Column(name = "_id")
+  private int id;
+  @Column(name = "NAME")
+  private String name;
+  @OneToOne
+  @JoinColumn(name = "COUNTRY_ID")
+  private Capital capital;
+  @OneToMany
+  @JoinColumn(name = "COUNTRY_ID")
+  private Collection<State> states;
+
+  public int getId() {
+    return id;
+  }
+
+  public void setId(int id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Capital getCapital() {
+    return capital;
+  }
+
+  public void setCapital(Capital capital) {
+    this.capital = capital;
+  }
+
+  public Collection<State> getStates() {
+    return states;
+  }
+
+  public void setStates(Collection<State> states) {
+    this.states = states;
+  }
+
+}
