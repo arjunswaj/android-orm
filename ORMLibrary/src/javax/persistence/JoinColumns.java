0ea9bfd9c67398585911bbdf7e724326cbff6b56
kumudinikakwani <kumudinikakwani13@gmail.com>
2014-03-17 21:54:46 -0400

    New annotations added.

--- ORMLibrary/src/javax/persistence/JoinColumns.java
+++ ORMLibrary/src/javax/persistence/JoinColumns.java
@@ -7,5 +7,7 @@ import java.lang.annotation.Target;
 
 @Target(value = ElementType.FIELD)
 @Retention(value = RetentionPolicy.RUNTIME)
-public @interface OneToOne {
+public @interface JoinColumns {
+	JoinColumn[] value();
+
 }
