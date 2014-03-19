bb48fd1f203f8562cd778e060daa5aaf67863eb8
kumudinikakwani <kumudinikakwani13@gmail.com>
2014-03-19 02:25:08 -0400

    Table Creation for ManyToOne mapping.

--- ORMLibrary/src/iiitb/dm/ormlibrary/ddl/impl/DDLStatementBuilderImpl.java
+++ ORMLibrary/src/iiitb/dm/ormlibrary/ddl/impl/DDLStatementBuilderImpl.java
@@ -1,5 +1,6 @@
 package iiitb.dm.ormlibrary.ddl.impl;
 
+import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -18,7 +19,7 @@ public class DDLStatementBuilderImpl implements DDLStatementBuilder
 {
 
 	private final String DDL_TAG = "DDL STATEMENT BUILDER";
-	Map<String, ClassDetails> classDetailsMap;
+	private Map<String, ClassDetails> classDetailsMap;
 
 
 	public DDLStatementBuilderImpl(Map<String, ClassDetails> classDetailsMap)
@@ -79,121 +80,138 @@ public class DDLStatementBuilderImpl implements DDLStatementBuilder
 						throw new MappingException("@JoinColumn missing on " + fieldTypeDetail.getFieldName() + " in " + classDetails.getClassName());
 				}
 			}
-			else{
-
-				columnName = fieldTypeDetail.getAnnotationOptionValues()
-						.get("Column").get("name");
-
-				columnType = SQLColTypeEnumMap.get(
-						fieldTypeDetail.getFieldType().getSimpleName()).toString();
-
-				if (fieldTypeDetail.getAnnotationOptionValues().get("Id") != null)
-				{
-					if(!columnName.equals("_id") || !columnType.equals("INTEGER"))
-						throw new MappingException("Column name must be _id and type must be INTEGER/INT " + fieldTypeDetail.getFieldName());
-					columnConstraints.add("PRIMARY KEY");
-					columnConstraints.add("AUTOINCREMENT");
-				}
-
-			}
-				if(doesColumnNameExist(columnName, columnsDescription))
-				throw new MappingException("Duplicate Columns in " + classDetails.getClassName());
-			if(!columnName.equals("") && !columnType.equals(""))
-				columnsDescription.add(new ColumnDescription(columnName, columnType, columnConstraints));
-
-		}
-
-		// Add a discriminator column if this class's inheritance strategy is JOINED.
-		if(classDetails.getAnnotationOptionValues().get("Inheritance") != null){
-			switch((InheritanceType)classDetails.getAnnotationOptionValues().get("Inheritance").get("strategy"))
+			else if(fieldTypeDetail.getAnnotationOptionValues().get("OneToMany") != null)
 			{
-			case JOINED:
-				columnName = (String)classDetails.getAnnotationOptionValues().get("DiscriminatorColumn").get("name");
-				columnType = "TEXT";
-				columnConstraints = new ArrayList<String>();
-				if(doesColumnNameExist(columnName, columnsDescription))
-					throw new MappingException("Duplicate Columns in " + classDetails.getClassName());
-				columnsDescription.add(new ColumnDescription(columnName, columnType, columnConstraints));
-				break;
-			case TABLE_PER_CLASS:
-			default:
-				break;
+				
+			}
+			else if(fieldTypeDetail.getAnnotationOptionValues().get("ManyToOne") != null)
+			{
+				columnName = fieldTypeDetail.getAnnotationOptionValues().get("JoinColumn").get("name");
+				foreignKeyConstraint.append(", FOREIGN KEY (" + columnName + ") REFERENCES "
+						+ classDetailsMap.get(fieldTypeDetail.getFieldType().getName()).getAnnotationOptionValues()
+						.get("Entity").get("name") + "(_id ) ");
+				Log.d(DDL_TAG, fieldTypeDetail.getFieldType().getName() + " " + classDetailsMap.get(fieldTypeDetail.getFieldType().getName()));
+				columnType = getColumnType(classDetailsMap.get(fieldTypeDetail.getFieldType().getName()).getFieldTypeDetails(), "_id");
 			}
 
-		}
+		else{
 
+			columnName = fieldTypeDetail.getAnnotationOptionValues()
+					.get("Column").get("name");
 
-		// Depending on this class's super class, either add foreign key or columns of super class.
-		if(superClassDetails != null && superClassDetails.getAnnotationOptionValues().get("Inheritance") != null){
-			switch((InheritanceType)superClassDetails.getAnnotationOptionValues().get("Inheritance").get("strategy"))
+			columnType = SQLColTypeEnumMap.get(
+					fieldTypeDetail.getFieldType().getSimpleName()).toString();
+
+			if (fieldTypeDetail.getAnnotationOptionValues().get("Id") != null)
 			{
-			case JOINED:
-				foreignKeyConstraint.append(" , FOREIGN KEY(_id) REFERENCES " 
-						+ superClassDetails.getAnnotationOptionValues().get("Entity").get("name")
-						+ "(_id)");
-				columnName = "_id";
-				columnType = "INTEGER";
-				columnConstraints = new ArrayList<String>();
+				if(!columnName.equals("_id") || !columnType.equals("INTEGER"))
+					throw new MappingException("Column name must be _id and type must be INTEGER/INT " + fieldTypeDetail.getFieldName());
 				columnConstraints.add("PRIMARY KEY");
 				columnConstraints.add("AUTOINCREMENT");
-				columnsDescription.add(new ColumnDescription(columnName, columnType, columnConstraints));
-				break;
-			case TABLE_PER_CLASS:
-			default:
-				superClassColumnDescription = ", " + superClassDetails.getColumnsDescription();
-				break;
-
 			}
-		}
 
-		String columnsDescriptionString = generateColumnDescriptionString(columnsDescription);
-		classDetails.setColumnsDescription(columnsDescriptionString);
-		String createStmt = "CREATE TABLE " + tableName + "( "
-				+ columnsDescriptionString + superClassColumnDescription + foreignKeyConstraint.toString() +  " )";
-		Log.d(DDL_TAG, createStmt);
-		return createStmt;
+		}
+			
+		if(columnNameExists(columnName, columnsDescription))
+			throw new MappingException("Duplicate Columns in " + classDetails.getClassName());
+		if(!columnName.equals("") && !columnType.equals(""))
+			columnsDescription.add(new ColumnDescription(columnName, columnType, columnConstraints));
+
 	}
 
-
-	private String getColumnType(List<FieldTypeDetails> fieldTypeDetailsList, String columnName)
-	{
-		String type = null;
-		for(FieldTypeDetails fieldTypeDetails: fieldTypeDetailsList)
+	// Add a discriminator column if this class's inheritance strategy is JOINED.
+	if(classDetails.getAnnotationOptionValues().get("Inheritance") != null){
+		switch((InheritanceType)classDetails.getAnnotationOptionValues().get("Inheritance").get("strategy"))
 		{
-			if(fieldTypeDetails.getAnnotationOptionValues().get("Column") != null
-					&& fieldTypeDetails.getAnnotationOptionValues().get("Column").get("name").equals(columnName))
-				type = SQLColTypeEnumMap.get(fieldTypeDetails.getFieldType().getSimpleName()).toString();
+		case JOINED:
+			columnName = (String)classDetails.getAnnotationOptionValues().get("DiscriminatorColumn").get("name");
+			columnType = "TEXT";
+			columnConstraints = new ArrayList<String>();
+			if(columnNameExists(columnName, columnsDescription))
+				throw new MappingException("Duplicate Columns in " + classDetails.getClassName());
+			columnsDescription.add(new ColumnDescription(columnName, columnType, columnConstraints));
+			break;
+		case TABLE_PER_CLASS:
+		default:
+			break;
 		}
 
-		return type;
 	}
 
-	private boolean doesColumnNameExist(String ColumnName, List<ColumnDescription> columnsDescription)
-	{
-		boolean result = false;
-		for(ColumnDescription columnDescription : columnsDescription)
+
+	// Depending on this class's super class, either add foreign key or columns of super class.
+	if(superClassDetails != null && superClassDetails.getAnnotationOptionValues().get("Inheritance") != null){
+		switch((InheritanceType)superClassDetails.getAnnotationOptionValues().get("Inheritance").get("strategy"))
 		{
-			if(columnDescription.getColumnName().equals(ColumnName))
-				result = true;
+		case JOINED:
+			foreignKeyConstraint.append(" , FOREIGN KEY(_id) REFERENCES " 
+					+ superClassDetails.getAnnotationOptionValues().get("Entity").get("name")
+					+ "(_id)");
+			columnName = "_id";
+			columnType = "INTEGER";
+			columnConstraints = new ArrayList<String>();
+			columnConstraints.add("PRIMARY KEY");
+			columnConstraints.add("AUTOINCREMENT");
+			if(columnNameExists(columnName, columnsDescription))
+				throw new MappingException("Duplicate Columns in " + classDetails.getClassName());
+			columnsDescription.add(new ColumnDescription(columnName, columnType, columnConstraints));
+			break;
+		case TABLE_PER_CLASS:
+		default:
+			superClassColumnDescription = ", " + superClassDetails.getColumnsDescription();
+			break;
+
 		}
-		return result;
 	}
 
-	private String generateColumnDescriptionString(List<ColumnDescription> columnsDescription)
+	String columnsDescriptionString = generateColumnDescriptionString(columnsDescription);
+	classDetails.setColumnsDescription(columnsDescriptionString);
+	String createStmt = "CREATE TABLE " + tableName + "( "
+			+ columnsDescriptionString + superClassColumnDescription + foreignKeyConstraint.toString() +  " )";
+	Log.d(DDL_TAG, createStmt);
+	return createStmt;
+}
+
+
+private String getColumnType(List<FieldTypeDetails> fieldTypeDetailsList, String columnName)
+{
+	String type = null;
+	for(FieldTypeDetails fieldTypeDetails: fieldTypeDetailsList)
 	{
-		StringBuilder query = new StringBuilder();
-		for(ColumnDescription columnDescription : columnsDescription)
-		{
-			query.append(columnDescription.getColumnName() + " " + columnDescription.getColumnType());
-			for(String constraint : columnDescription.getColumnConstraints())
-				query.append(" " + constraint + " ");
-			query.append(", ");
-		}
-		query.delete(query.length() - 2, query.length() - 1);
-		return query.toString();
-
+		if(fieldTypeDetails.getAnnotationOptionValues().get("Column") != null
+				&& fieldTypeDetails.getAnnotationOptionValues().get("Column").get("name").equals(columnName))
+			type = SQLColTypeEnumMap.get(fieldTypeDetails.getFieldType().getSimpleName()).toString();
 	}
-	
+
+	return type;
+}
+
+private boolean columnNameExists(String ColumnName, List<ColumnDescription> columnsDescription)
+{
+	boolean result = false;
+	for(ColumnDescription columnDescription : columnsDescription)
+	{
+		if(columnDescription.getColumnName().equals(ColumnName))
+			result = true;
+	}
+	return result;
+}
+
+private String generateColumnDescriptionString(List<ColumnDescription> columnsDescription)
+{
+	StringBuilder query = new StringBuilder();
+	for(ColumnDescription columnDescription : columnsDescription)
+	{
+		query.append(columnDescription.getColumnName() + " " + columnDescription.getColumnType());
+		for(String constraint : columnDescription.getColumnConstraints())
+			query.append(" " + constraint + " ");
+		query.append(", ");
+	}
+	query.delete(query.length() - 2, query.length() - 1);
+	return query.toString();
+
+}
+
 
 
 }
